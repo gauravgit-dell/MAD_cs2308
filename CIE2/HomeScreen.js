5d412b880a1bc8f352f88258843d8ee92f83d21c
GAURAV P RAO <2REACHGPR@GMAIL.COM>
2026-03-23 15:38:04 +0530

    Add navigation stack with Home and CreateAccount screens

---
+++ CIE2/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./HomeScreen";
+import CreateAccountScreen from "./CreateAccountScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="CreateAccount"
+          component={CreateAccountScreen}
+          options={{ title: "Create Account" }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
